b93069f0240e550362430060cc1e231df2c816f2
Samuel Cavalcante <samuel-icm-dantas@hotmail.com>
2024-02-27 17:41:38 -0300

    feat: add packages

--- plugins/community.lua
+++ plugins/community.lua
@@ -3,7 +3,14 @@ return {
   "AstroNvim/astrocommunity",
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
-
+  
   -- { import = "astrocommunity.colorscheme.catppuccin" },
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  {import = "astrocommunity.pack.typescript"},
+  {import = "astrocommunity.pack.html-css"},
+  {import = "astrocommunity.pack.tailwindcss"},
+  {import = "astrocommunity.lsp.lsp-inlayhints-nvim"},
+  {import = "astrocommunity.colorscheme.dracula-nvim"},
+  {import = "astrocommunity.colorscheme.tokyonight-nvim"},
+  {import = "astrocommunity.colorscheme.catppuccin"}
 }
--- plugins/user.lua
+++ plugins/user.lua
@@ -9,4 +9,29 @@ return {
   --     require("lsp_signature").setup()
   --   end,
   -- },
+  {
+    "rebelot/terminal.nvim",
+    opts = {} 
+  },
+  {
+    "numToStr/FTerm.nvim",
+    opts = {}
+  },
+  {
+    "jellydn/typecheck.nvim",
+  dependencies = { "folke/trouble.nvim", dependencies = { "nvim-tree/nvim-web-devicons" } },
+  ft = { "javascript", "javascriptreact", "json", "jsonc", "typescript", "typescriptreact" },
+  opts = {
+    debug = true,
+    mode = "trouble", -- "quickfix" | "trouble"
+  },
+  keys = {
+    {
+      "<leader>ck",
+      "<cmd>Typecheck<cr>",
+      desc = "Run Type Check",
+    },
+  }
+  }
+
 }
